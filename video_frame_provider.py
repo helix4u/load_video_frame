a9e632c1e5660c409b406735ee2be32dd7d68ccb
Gille <btgille@gmail.com>
2023-08-07 17:02:05 -0600

    SkunkWorxDark contributed changes integrated.
    https://discord.com/channels/1020123559063990373/1136905319839174728 for more information.

--- video_frame_provider.py
+++ video_frame_provider.py
@@ -1,6 +1,9 @@
 from typing import Literal, Optional
 from PIL import Image
 import cv2
+import json
+import numpy as np
+
 from pydantic import Field
 from ..models.image import (
     ImageCategory,
@@ -12,6 +15,7 @@ from .baseinvocation import (
     BaseInvocation,
     InvocationContext,
     InvocationConfig,
+    BaseInvocationOutput,
 )
 
 
@@ -65,3 +69,60 @@ class LoadVideoFrameInvocation(BaseInvocation):
             width=image_dto.width,
             height=image_dto.height,
         )
+
+
+class ImageIndexCollectOutput(BaseInvocationOutput):
+    """XImageCollectOutput string containg an array of xItem, Image_name converted to json"""
+    type: Literal["image_index_collect_output"] = "image_index_collect_output"
+    image_index_collection: str = Field(description="The Image Index Collection ")
+
+    class Config:
+        schema_extra = {'required': ['type','ximage']}
+
+class ImageIndexCollectInvocation(BaseInvocation):
+    """ImageIndexCollect takes Image and Index then outputs it as an (index,image_name)array converted to json"""
+    type: Literal["image_index_collect"] = "image_index_collect"
+    index: int = Field(default=0, description="The index")
+    image: ImageField = Field(default=None, description="The image associated with the index")
+
+    class Config(InvocationConfig):
+        schema_extra = {"ui": {"title": "Image Index Collect"}}
+
+    def invoke(self, context: InvocationContext) -> ImageIndexCollectOutput:
+        """Invoke with provided services and return outputs."""
+        return ImageIndexCollectOutput(image_index_collection = json.dumps([self.index , self.image.image_name]))
+    
+
+class ImagesIndexToVideoOutput(BaseInvocationOutput):
+    """ImagesIndexToVideoOutput returns nothing"""
+    type: Literal["image_index_to_video_output"] = "image_index_to_video_output"
+
+    class Config:
+        schema_extra = {'required': ['type']}  
+
+class ImagesIndexToVideoInvocation(BaseInvocation):#, PILInvocationConfig):
+    """Load a collection of xyimage types (json of (x_item,y_item,image_name)array) and create a gridimage of them"""
+    type: Literal["image_index_to_video"] = "image_index_to_video"
+    image_index_collection: list[str] = Field(default=[], description="The Image Index Collection")
+    video_out_path: str = Field(default='', description="Path and filename of output mp4")
+    fps: int = Field(default=30, description="FPS")
+
+    class Config(InvocationConfig):
+        schema_extra = {"ui": {"title": "Image Index To Video"}}
+  
+    def invoke(self, context: InvocationContext) -> ImagesIndexToVideoOutput:
+        """Convert an image list a video"""
+
+        new_array = sorted([json.loads(s) for s in self.image_index_collection], key=lambda x: x[0])
+        image = context.services.images.get_pil_image(new_array[0][1])
+        
+        video_writer = cv2.VideoWriter(self.video_out_path, cv2.VideoWriter_fourcc(*'mp4v'), self.fps, (image.width, image.height))
+
+        for item in new_array:
+            image = context.services.images.get_pil_image(item[1])
+            image = np.array(image)
+            video_writer.write(image)
+
+        video_writer.release()
+
+        return ImagesIndexToVideoOutput()
