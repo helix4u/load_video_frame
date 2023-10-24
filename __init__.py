3a34149baacd83d576e8be7fc476acd6f38cf3c0
Gille <btgille@gmail.com>
2023-10-23 21:04:22 -0600

    3.4 support

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .video_frame_provider import *
--- video_frame_provider.py
+++ video_frame_provider.py
@@ -6,34 +6,26 @@ from pathlib import Path
 from PIL import Image, ImageChops, ImageFilter, ImageOps
 from invokeai.app.invocations.primitives import ColorField, ImageField, ImageOutput, IntegerOutput
 from pydantic import Field
-from ..models.image import ImageCategory, ResourceOrigin
-from .baseinvocation import BaseInvocation, FieldDescriptions, InputField, InvocationContext, invocation, BaseInvocationOutput, UIComponent, invocation_output, OutputField, UIType
+from invokeai.app.services.image_records.image_records_common import ImageCategory, ResourceOrigin
+from invokeai.app.invocations.baseinvocation import (
+    BaseInvocation,
+    BaseInvocationOutput,
+    FieldDescriptions,
+    Input,
+    InputField,
+    InvocationContext,
+    OutputField,
+    UIComponent,
+    UIType,
+    WithMetadata,
+    WithWorkflow,
+    invocation,
+    invocation_output,
+)
 
 """
-ImagesIndexToVideoInvocation and it's output still need to be sorted out but does function as-is.
-
-Notes: 
-
-the @invocation decorator replaces  type: Literal.
-it's first argument is that value.
-for example, @invocation("load_video_frame" technically is the same as type: Literal but more succinct and lets us change any implementation details as needed without impacting functionality of the node.
-same for outputs.
-
-use @invocation_output("output_type") ()) instead of type
-
-class Config is totally replaced by the decorator. you can omit this on all nodes.
-
-suggest to include workflow=self.workflow in the call to context.services.images.create(). this allows the images created by that node to embed the workflow into them. this checkbox will be displayed on every node that outputs an image
-
-all outputs should use OutputField instead of Field
-
-Todo:
-Translate ImagesIndexToVideoOutput and it's output into invocation decorator speak using notes and links as context:
-
-links for reading the how:
-invokeai/app/invocations/primitives.py
-invokeai/app/invocations/baseinvocation.py
-a.k.a. if it doesnt make sense, just go look at working examples and adapt from there.
+Note: I always hate code breaking changes! XD
+If work need to be made for others, clarity on when and why the change is going to be made and the advantages of the change for the user-base of the product would be cool. Otherwise it kinda just seems like we are just iterating for the sake of iterating. I don't live in the code and so consolidating docs into something digestable for someone not directly involved in the day to day project upkeep may be advantagious to maintain so that contributers can understand the changes without having to search a lot of text or find a thread. Somewhere that knowledge is consolidated in a presentation manner instead of tucked away beneath discord conversation and site fractured documantation stores.
 """
 
 @invocation(
@@ -41,7 +33,7 @@ a.k.a. if it doesnt make sense, just go look at working examples and adapt from 
     title="Load Video Frame", 
     tags=["video", "load", "frame"], 
     category="image",
-    version="1.0.0",
+    version="1.0.1",
 )
 class LoadVideoFrameInvocation(BaseInvocation):
     """Load a specific frame from an MP4 video and provide it as output."""
@@ -76,7 +68,6 @@ class LoadVideoFrameInvocation(BaseInvocation):
             node_id=self.id,
             session_id=context.graph_execution_state_id,
             is_intermediate=self.is_intermediate,
-            workflow=self.workflow,
         )
 
         return ImageOutput(
@@ -97,11 +88,10 @@ class ImageIndexCollectOutput(BaseInvocationOutput):
     title="Image Index Collect", 
     tags=["video", "collection", "image", "index", "frame", "collection"], 
     category="collections",
-    version="1.0.0",
+    version="1.0.1",
 )
 class ImageIndexCollectInvocation(BaseInvocation):
     """ImageIndexCollect takes Image and Index then outputs it as an (index,image_name)array converted to json"""
-    type: Literal["image_index_collect"] = "image_index_collect"
     index: int = InputField(default=0, description="The index")
     image: ImageField = InputField(default=None, description="The image associated with the index")
 
@@ -109,22 +99,23 @@ class ImageIndexCollectInvocation(BaseInvocation):
         """Invoke with provided services and return outputs."""
         return ImageIndexCollectOutput(image_index_collection = json.dumps([self.index , self.image.image_name]))
 
-  
+@invocation_output("ImagesIndexToVideoOutput")  
 class ImagesIndexToVideoOutput(BaseInvocationOutput):
     """ImagesIndexToVideoOutput returns nothing"""
-    type: Literal["image_index_to_video_output"] = "image_index_to_video_output"
 
-    class Config:
-        schema_extra = {'required': ['type']}  
-
+@invocation(
+    "ImagesIndexToVideoOutput", 
+    title="Images Index To Video Output", 
+    tags=["video", "collection", "image", "index", "frame", "collection"], 
+    category="collections",
+    version="1.0.1",
+)
 class ImagesIndexToVideoInvocation(BaseInvocation):#, PILInvocationConfig):
     """Load a collection of xyimage types (json of (x_item,y_item,image_name)array) and create a gridimage of them"""
-    type: Literal["image_index_to_video"] = "image_index_to_video"
     image_index_collection: list[str] = InputField(default_factory=list, description="The Image Index Collection", ui_type=UIType.ImageCollection)
     video_out_path: str = InputField(default='', description="Path and filename of output mp4")
     fps: int = InputField(default=30, description="FPS")
 
-  
     def invoke(self, context: InvocationContext) -> ImagesIndexToVideoOutput:
         """Convert an image list a video"""
         new_array = sorted([json.loads(s) for s in self.image_index_collection], key=lambda x: x[0])
@@ -147,7 +138,7 @@ class ImagesIndexToVideoInvocation(BaseInvocation):#, PILInvocationConfig):
     title="Get Total Frames", 
     tags=["video", "frames", "count"], 
     category="video",
-    version="1.0.0",
+    version="1.0.1",
 )
 class GetTotalFramesInvocation(BaseInvocation):
     """Get the total number of frames in an MP4 video and provide it as output."""
@@ -172,7 +163,7 @@ class GetTotalFramesInvocation(BaseInvocation):
     title="Get Source Frame Rate", 
     tags=["video", "framerate"], 
     category="video",
-    version="1.0.0",
+    version="1.0.1",
 )
 class GetSourceFrameRateInvocation(BaseInvocation):
     """Get the source framerate of an MP4 video and provide it as output."""
